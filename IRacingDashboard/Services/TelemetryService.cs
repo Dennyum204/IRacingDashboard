4aa4be9a5ede780feaa4c7d6aa747fd09f615d8c
Fernando Pereira <fernando.dbp@hotmail.com>
2025-04-26 00:14:36 +0200

    Add project files.

---
+++ IRacingDashboard/Services/TelemetryService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Timers;
+using irsdkSharp;
+using irsdkSharp.Serialization;
+using irsdkSharp.Serialization.Models.Data;
+using irsdkSharp.Serialization.Models.Fastest;
+
+namespace IRacingDashboard.Services
+{
+    public class TelemetryService
+    {
+        private readonly IRacingSDK _irsdk;
+        private readonly System.Timers.Timer _pollingTimer;
+        private readonly double _pollingIntervalMs = 16.6;
+
+        public event Action<Data> TelemetryUpdated;
+        public event Action<bool> ConnectionChanged;
+
+
+        public TelemetryService()
+        {
+            _irsdk = new IRacingSDK();
+            _pollingTimer = new System.Timers.Timer(_pollingIntervalMs);
+            _pollingTimer.Elapsed += PollingTimerElapsed;
+        }
+
+        public void Start()
+        {
+            _pollingTimer.Start();
+        }
+
+        public void Stop()
+        {
+            _pollingTimer.Stop();
+        }
+
+        private void PollingTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            bool connected = _irsdk.IsConnected();
+
+            ConnectionChanged?.Invoke(connected); // ✅ Tell the ViewModel the connection status
+
+
+            if (connected)
+            {
+                var telemetry = _irsdk.GetData();
+                if (telemetry != null)
+                {
+                    TelemetryUpdated?.Invoke(telemetry);
+                }
+            }
+        }
+    }
+}
---
+++ IRacingDashboard/ViewModels/MainViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using IRacingDashboard.Services;
+using irsdkSharp.Serialization.Models.Fastest;
+using System.Collections.Generic;
+using System.Windows.Media;
+
+namespace IRacingDashboard.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private readonly TelemetryService _telemetryService;
+        public string ConnectionStatus => IsConnected ? "Connected to iRacing" : "Waiting for iRacing...";
+        public Brush ConnectionColor => IsConnected ? Brushes.Green : Brushes.Red;
+        public ObservableCollection<ISeries> Series { get; set; }
+        public List<Axis> XAxes { get; set; }
+        public List<Axis> YAxes { get; set; }
+
+        private List<double> _throttleValues = new();
+        private List<double> _brakeValues = new();
+        private int _time = 0;
+
+        public MainViewModel()
+        {
+            Series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<double> { Name = "Throttle", Values = _throttleValues },
+                new LineSeries<double> { Name = "Brake", Values = _brakeValues }
+            };
+
+            XAxes = new List<Axis> { new Axis { Name = "Time (s)" } };
+            YAxes = new List<Axis> { new Axis { Name = "Input", MinLimit = 0, MaxLimit = 1 } };
+
+            _telemetryService = new TelemetryService();
+            _telemetryService.TelemetryUpdated += OnTelemetryUpdated;
+            _telemetryService.ConnectionChanged += OnConnectionChanged; // ✅ Listen for connection changes
+            _telemetryService.Start();
+        }
+
+        private void OnConnectionChanged(bool connected)
+        {
+            IsConnected = connected;
+        }
+        private void OnTelemetryUpdated(Data telemetry)
+        {
+            App.Current.Dispatcher.Invoke(() =>
+            {
+                _time++;
+
+                if (_throttleValues.Count > 100)
+                {
+                    _throttleValues.RemoveAt(0);
+                    _brakeValues.RemoveAt(0);
+                }
+
+                _throttleValues.Add(telemetry.Throttle);
+                _brakeValues.Add(telemetry.Brake);
+            });
+        }
+
+        #region Properties
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set { _isConnected = value; OnPropertyChanged(); 
+                OnPropertyChanged(nameof(ConnectionStatus));
+                OnPropertyChanged(nameof(ConnectionColor));
+            }
+        }
+        #endregion
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
